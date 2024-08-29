940219a8494114da3dae113be3e59dd6c4d5478a
lauramtirado <alskling9@gmail.com>
2024-08-28 23:22:38 -0500

    feat: Reestructured all prior project into Maven config
    Prior files were used as structure and were reconfigured to be used with Maven

---
+++ src/test/java/finalproject/com/Clinica_Odontologica/ClinicaOdontologicaApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.com.Clinica_Odontologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaOdontologicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
