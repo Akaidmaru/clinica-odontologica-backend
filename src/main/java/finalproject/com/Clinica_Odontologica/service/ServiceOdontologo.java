563123b8b1a0763bf4e7f31aed251a7b5eef889a
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2024-09-04 21:50:42 -0500

    feat: SeviceOdontologo
    Updated of ServiceOdontologo

--- src/main/java/finalproject/com/Clinica_Odontologica/service/ServiceOdontologo.java
+++ src/main/java/finalproject/com/Clinica_Odontologica/service/ServiceOdontologo.java
@@ -1,20 +1,64 @@
-package Odontologo.com.Empresa.service;
+package finalproject.com.Clinica_Odontologica.service.impl;
 
-import Odontologo.com.Empresa.dao.IDao;
-import Odontologo.com.Empresa.model.Odontologo;
+import finalproject.com.Clinica_Odontologica.entity.Odontologo;
+import finalproject.com.Clinica_Odontologica.respository.IOdontologoRepository;
+import finalproject.com.Clinica_Odontologica.service.IOdontologoService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-public class ServiceOdontologo {
-    private IDao<Odontologo> odontologoIDao;
-    public ServiceOdontologo(IDao<Odontologo> odontologoIDao){
-        this.odontologoIDao = odontologoIDao;
-    }
-    public Odontologo save(Odontologo odontologo){
-        return odontologoIDao.save(odontologo);
+@Service
+public class ServiceOdontologo implements IOdontologoService {
+
+    private IOdontologoService odontologoService;
+
+    public ServiceOdontologo(IOdontologoRepository IOdontologoRepository) {
+        this.odontologoService = IOdontologoService;
     }
 
-    public List<Odontologo> findAll(){
-        return odontologoIDao.findAll();
+    @Override
+    public Odontologo guardarOdontologo(Odontologo odontologo){
+        return odontologoRepository.save(odontologo);
+    }
+
+    @Override
+    public Optional<Odontologo> buscarId(Integer id) {
+        return odontologoRepository.findById(id);
+    }
+
+        @Override
+    public List<Odontologo> buscarTodos() {
+        return odontologoRepository.findAll();
+    }
+
+    @Override
+    public void modificarOdontologo(Odontologo odontologo){
+        odontologoRepository.save(odontologo);  //FIX: MAYBE RETURN NEEDED
+    }
+
+    @Override
+    public void eliminarOdontologo(Integer id) {
+        odontologoRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Odontologo> buscarApellido(String apellido) {
+        return odontologoRepository.findByFullName(appelido);
+    }
+
+    @Override
+    public List<Odontologo> buscarNombre(String nombre) {
+        return odontologoRepository.findByFullName(nombre);
+    }
+
+    @Override
+    public List<Odontologo> buscarApellidoNombre(String apellido, String nombre) {
+        return odontologoRepository.findByFullName(appelido, nombre);
+    }
+
+    @Override
+    public Optional<Odontologo> buscarMatricula(String matricula) {
+        return odontologoRepository.findByMatricula(matricula);
     }
 }
