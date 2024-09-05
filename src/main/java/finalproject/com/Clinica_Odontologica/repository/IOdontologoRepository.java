9ebe7cdfe91c8bab3aeb896f2a2509d3f70169c8
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2024-09-04 22:44:27 -0500

    feat: IOdontologoService
    Created IOdontologoService

---
+++ src/main/java/finalproject/com/Clinica_Odontologica/repository/IOdontologoRepository.java
@@ -0,0 +1,26 @@
+package finalproject.com.Clinica_Odontologica.repository;
+
+import finalproject.com.Clinica_Odontologica.entity.Odontologo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface IOdontologoRepository extends JpaRepository<Odontologo, Integer>{
+    List<Odontologo> buscarApellidoNombre(String apellido, String nombre);
+
+    @Query("SELECT P FROM ODONTOLOGO P WHERE P.NOMBRE LIKE %:NOMBRE%")
+    List<Odontologo> buscarNombre(String nombre);
+
+    @Query("SELECT P FROM ODONTOLOGO P WHERE P.APELLIDO LIKE %:APELLIDO%")
+    List<Odontologo> buscarApellido(String apellido);
+
+    Optional<Odontologo> buscarMatricula(String matricula); //FIX
+
+
+
+
+}
