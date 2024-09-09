0797c08a01c29cbd14210ce8f974b86a561da519
lauramtirado <alskling9@gmail.com>
2024-09-09 15:47:25 -0500

    feat: Created pacienteController
    Created the file for pacienteController:
    - Save
    - Modify
    - Search
    - Search all
    - Search by name
    - Search by last name
    - Search by ID
    - Search by DNI
    - Delete

---
+++ src/main/java/finalproject/com/Clinica_Odontologica/controller/pacienteController.java
@@ -0,0 +1,99 @@
+package finalproject.com.Clinica_Odontologica.controller;
+
+import finalproject.com.Clinica_Odontologica.entity.Odontologo;
+import finalproject.com.Clinica_Odontologica.entity.Paciente;
+import finalproject.com.Clinica_Odontologica.service.impl.ServicePaciente;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/paciente")
+public class pacienteController {
+
+    private ServicePaciente servicePaciente;
+
+    public pacienteController(ServicePaciente servicePaciente) {
+        this.servicePaciente = servicePaciente;
+    }
+
+    @PostMapping("/guardar")
+    public ResponseEntity<Paciente> guardarPaciente(@RequestBody Paciente paciente){
+        return ResponseEntity.ok(servicePaciente.guardarPaciente(paciente));
+    }
+
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Paciente> buscarId(@PathVariable Integer id){
+        Optional<Paciente> paciente = servicePaciente.buscarId(id);
+        if (paciente.isPresent()){
+            return ResponseEntity.ok(paciente.get());
+        }
+        else {
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        }
+    }
+
+    @GetMapping("/buscartodos")
+    public ResponseEntity<List<Paciente>> buscarTodos(){
+        return ResponseEntity.ok(servicePaciente.buscarTodos());
+    }
+
+    @PutMapping("/modificar")
+    public ResponseEntity<?> modificarPaciente(@RequestBody Paciente paciente) {
+        Optional<Paciente> pacienteEncontrado = servicePaciente.buscarId(paciente.getId());
+        if (pacienteEncontrado.isPresent()) {
+            servicePaciente.modificarPaciente(pacienteEncontrado.get());
+            String jsonResponse = "{\"mensaje\": \"El paciente ha sido modificado\"}";
+            return ResponseEntity.ok(jsonResponse);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> eliminarPaciente(@PathVariable Integer id)
+    {Optional<Paciente> pacienteAEliminar = servicePaciente.buscarId(id);
+        if (pacienteAEliminar.isPresent()){
+            servicePaciente.eliminarPaciente(id);
+            String jsonResponse = "{\"mensaje\": \"El paciente ha sido eliminado\"}";
+            return ResponseEntity.ok(jsonResponse);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscarApellido/{apellido}")
+    public ResponseEntity<List<Paciente>> buscarApellido(@PathVariable String apellido){
+        return ResponseEntity.ok(servicePaciente.buscarApellido(apellido));
+
+    }
+
+    @GetMapping("/buscarNombre/{nombre}")
+    public ResponseEntity<List<Paciente>> buscarNombre(@PathVariable String nombre){
+        return ResponseEntity.ok(servicePaciente.buscarNombre(nombre));
+
+    }
+
+    @GetMapping("/buscarApellidoNombre/{apellido}/{nombre}")
+    public ResponseEntity<List<Odontologo>> buscarApellidoNombre(@RequestParam String apellido, String nombre){
+        return ResponseEntity.ok(servicePaciente.buscarApellidoNombre(apellido,nombre));
+
+    }
+
+    @GetMapping("/buscar/{dni}")
+    public ResponseEntity<Optional<Paciente>> buscarDni(@PathVariable String dni){
+        Optional<Paciente> pacienteEncontrado = servicePaciente.buscarDni(dni);
+        if (pacienteEncontrado.isPresent()){
+            return ResponseEntity.ok(servicePaciente.buscarDni(dni));
+        }
+        else {
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        }
+    }
+
+
+}
