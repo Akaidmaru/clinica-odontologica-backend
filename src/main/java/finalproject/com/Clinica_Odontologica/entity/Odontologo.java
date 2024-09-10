08a8f3feb40f0e35852a310d397a52ff8d407f28
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2024-09-10 13:09:11 -0500

    fix: Missing import
    Fixed import, jakarta.persistence.* is needed in order to code to function.

--- src/main/java/finalproject/com/Clinica_Odontologica/entity/Odontologo.java
+++ src/main/java/finalproject/com/Clinica_Odontologica/entity/Odontologo.java
@@ -2,10 +2,12 @@ package finalproject.com.Clinica_Odontologica.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 import java.util.Set;
 
